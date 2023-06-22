5dc1a1b357c56ac25779536c89fbb238ce3c417f
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2023-06-22 18:32:18 +1200

    Add fetchRandomWord function

--- script.ts
+++ script.ts
@@ -1,1 +1,10 @@
-window.addEventListener('load', function () {});
+window.addEventListener('load', function () {
+    async function fetchRandomWord() {
+        const res = await fetch(
+            'https://random-word-api.vercel.app/api?words=1&type=capitalized'
+        );
+        const randomWord = await res.json();
+
+        return randomWord[0];
+    }
+});
